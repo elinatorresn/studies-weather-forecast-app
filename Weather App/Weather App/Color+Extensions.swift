a67b588e95a7865215e1c5c7a556004777b3f836
Elina Torres <154446327+elinatorresn@users.noreply.github.com>
2024-06-01 11:37:16 -0300

    add HourlyForecastCollectionViewCell and Color+Extensions

---
+++ Weather App/Weather App/Color+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Extensions.swift
+//  Weather App
+//
+//  Created by Elina Torres on 01/06/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let contrastColor = UIColor(named: "contrastColor")
+    static let primaryColor = UIColor(named: "primaryColor")
+    static let graySoft = UIColor(named: "softGray")
+    
+}
