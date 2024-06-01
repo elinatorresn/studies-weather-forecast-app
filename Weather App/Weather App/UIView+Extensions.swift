d08f2fa1cf5ad1f4f86d19d9f5b645bed6216a7e
Elina Torres <154446327+elinatorresn@users.noreply.github.com>
2024-06-01 14:16:48 -0300

    add dailyForecastTableView

---
+++ Weather App/Weather App/UIView+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  UIView+Extensions.swift
+//  Weather App
+//
+//  Created by Elina Torres on 01/06/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    func setConstraintsToParent(_ parent: UIView) {
+        NSLayoutConstraint.activate([
+            self.topAnchor.constraint(equalTo: parent.topAnchor),
+            self.bottomAnchor.constraint(equalTo: parent.bottomAnchor),
+            self.leadingAnchor.constraint(equalTo: parent.leadingAnchor),
+            self.trailingAnchor.constraint(equalTo: parent.trailingAnchor)
+        ])
+    }
+}
